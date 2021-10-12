53d1f37644a25923ff11ad19a7e928622aeb6476
Josh Leigh <joshleigh00@gmail.com>
2021-10-12 12:27:20 -0600

    Worked on JS

--- starwars/starwars-film.js
+++ starwars/starwars-film.js
@@ -1,1 +1,14 @@
-import { films } from '../data/film.js'
+import { films } from '../data/films.js'
+
+
+let filmlist = document.querySelector('#filmlist')
+
+let titleList = document.createElement('ol')
+
+filmlist.appendChild(titleList)
+
+for (let i = 0; i < films.length; i++) {
+    let titleItem = document.createElement('li')
+    titleItem.textContent = films[i].title
+    titleList.appendChild(titleItem)
+}
