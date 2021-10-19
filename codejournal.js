1fbb0b5ec59b8ffc009ed629f6e64d42fb0dec88
Josh Leigh <joshleigh00@gmail.com>
2021-10-19 12:40:06 -0600

    started character page

--- codejournal.js
+++ codejournal.js
@@ -65,3 +65,12 @@ Funtion sumTwoThings(one,two) {
     //watch out for data type issues here;
     return one + two; //if numbers, will add them. If strings, will concatenate.
 }
+
+//Arrow Functions
+
+element => console.log(element) // implicit 'return' when only on lin for the function
+element => {
+    let foo = 'bar' + 'baxz'
+    return console.log(element) //explicit 'return' because of multiple lines
+}
+(num1, num2) => num1 = num2
