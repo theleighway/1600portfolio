3b4b3d322133e58bcf70d3c6b0e364c428cde722
Josh Leigh <joshleigh00@gmail.com>
2021-10-21 11:51:08 -0600

    updating string

--- characters/sw-characters.js
+++ characters/sw-characters.js
@@ -4,14 +4,14 @@ const main = document.querySelector("#main");
 
 console.log(people.length);
 
-people.forEach((element, index) => {
+people.forEach((element) => {
   const personFig = document.createElement("figure");
   const personImg = document.createElement("img");
   personImg.src = `https://starwars-visualguide.com/assets/img/characters/${
     index + 1
   }.jpg`;
   const personCaption = document.createElement("figcaption");
-  personCaption.textContent = "Person name goes here";
+  personCaption.textContent = element.name;
 
   personFig.appendChild(personImg);
   personFig.appendChild(personCaption);
