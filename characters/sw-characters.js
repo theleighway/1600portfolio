13b637b155b7d99e4ef770f9d45ac61f85d48e86
Josh Leigh <joshleigh00@gmail.com>
2021-10-26 12:44:31 -0600

    Added some js magic that added buttons and fixed fig captions

--- characters/sw-characters.js
+++ characters/sw-characters.js
@@ -1,21 +1,57 @@
 import { people } from "../data/people.js";
+import {getLastNumber} from '../utils/index.js'
 
 const main = document.querySelector("#main");
 
+const mainHeader = document.createElement('header')
+document.body.insertBefore(mainHeader, main)
+
+//begining of all button
+const allButton = document.createElement('button')
+allButton.textContent = 'All Characters'
+allButton.addEventListener('click', () => populateDOM(people))
+mainHeader.appendChild(allButton)
+
+//begining of male button
+const maleButton = document.createElement('button')
+maleButton.textContent = 'Male Characters'
+maleButton.addEventListener('click', () => populateDOM(maleCharacters))
+mainHeader.appendChild(maleButton)
+
+// begining of female button
+const femaleButton = document.createElement('button')
+femaleButton.textContent = 'Female Characters'
+femaleButton.addEventListener('click', () => populateDOM(femaleCharacters))
+mainHeader.appendChild(femaleButton)
+
+//begining of other button
+const otherButton = document.createElement('button')
+otherButton.textContent = 'Other Characters'
+otherButton.addEventListener('click', () => populateDOM(otherCharacters))
+mainHeader.appendChild(otherButton)
+
+
+
 const maleCharacters = people.filter(person => person.gender === 'male')
-console.log(maleCharacters.length)
+
 const femaleCharacters = people.filter(person => person.gender === 'female')
 console.log(femaleCharacters.length)
 
 const otherCharacters = people.filter(person => {
-  if(person.gender === 'n/a' || person.gender === 'hermaphrodite') {
+  if(person.gender === 'n/a' || person.gender === 'hermaphrodite' || person.gender === 'none') {
     return person
   }
 })
 
-console.log(otherCharacters)
 
-people.forEach((element) => {
+function populateDOM(characters) {
+  //remove all the previous items before pupulating with new ones
+while (main.firstChild) {
+  main.removeChild(main.firstChild)
+}
+
+
+characters.forEach((element) => {
   const personFig = document.createElement("figure");
   const personImg = document.createElement("img");
   let charNum = getLastNumber(element.url)
@@ -28,14 +64,8 @@ people.forEach((element) => {
 
   main.appendChild(personFig);
   
-});
+})
+}
 
-function getLastNumber(url) {
-  let end = url.lastIndexOf('/')
-  let start = end - 2
-  if (url.charAt(start) === '/') {
-    start++
-  }
-  return url.slice(start, end)
-};
+
 // I looked over the js code today to try an figure out more on what it does. Jv is still pretty hard for me.
