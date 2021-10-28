053144c16866cf77054e0ee237f44d50d8821c48
Josh Leigh <joshleigh00@gmail.com>
2021-10-28 12:04:55 -0600

    began starship page

---
+++ starships/sw-starships.js
@@ -0,0 +1,6 @@
+import { starships } from "../data/starships.js";
+
+const nav = document.querySelector('.nav')
+const navList = document.querySelector('.navList')
+const shipView = document.querySelector('.shipViewer')
+
