26b2fa575d941d3d6e3a3bfc745072dcf6195d0c
Josh Leigh <joshleigh00@gmail.com>
2021-11-09 12:43:11 -0700

    Added simplifiedSenators and party filter

--- congress/congress.js
+++ congress/congress.js
@@ -4,28 +4,42 @@ const senatorDiv = document.querySelector('.senators')
 
 function simplifiedSenators(senatorArray) {
   return senatorArray.map((senator) => {
+    const middleName = senator.middle_name ? ` ${senator.middle_name} ` : ` `
     return {
       id: senator.id,
-      name:senator.first_name,
-      party: senator.party
-
+      name:`${senator.first_name}${middleName}${senator.last_name}`,
+      party: senator.party,
+      imgURL: `https://www.govtrack.us/static/legislator-photos/${senator.govtrack_id}-100px.jpeg`,
+      gender: senator.gender,
+      seniority: "later",
+      missedVotesPct: senator.missed_votes_pct,
+      loyaltyPct: senator.votes_with_party_pct,
     }
   })
 }
 
-console.log(simplifiedSenators(senators))
+populateSenatorDiv(simplifiedSenators(senators))
 
-function populateSenatorDiv() {
-  senators.forEach(senator => {
+function populateSenatorDiv(simpleSenators) {
+  simpleSenators.forEach(senator => {
     let senFigure = document.createElement('figure')
     let figImg = document.createElement('img')
     let figCaption = document.createElement('figcaption')
 
-    figCaption.textContent = senator.first_name
+    figImg.src = senator.imgURL
+
+    figCaption.textContent = senator.name
     senFigure.appendChild(figImg)
     senFigure.appendChild(figCaption)
     senatorDiv.appendChild(senFigure)
   })
 }
 
-populateSenatorDiv()
+
+const filterSenators = (prop, value) => {
+  return simplifiedSenators(senators).filter(senator => senator[prop] === value)
+}
+
+const republicans = filterSenators('party', 'R')
+const femaleSenators = filterSenators('gender', 'F')
+
