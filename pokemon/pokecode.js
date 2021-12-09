fec81cd259abcbef237974ed66b862139ef3343e
Josh Leigh <joshleigh00@gmail.com>
2021-12-09 12:47:44 -0700

    tried to fix fire button

--- pokemon/pokecode.js
+++ pokemon/pokecode.js
@@ -7,14 +7,7 @@ function getAPIData(url) {
     console.error(error)
   }
 }
-async function filterData(apidata,filter){
-  result = await filter()
-}
 
-async function filter(arr, callback) {
-  const fail = Symbol()
-  return (await Promise.all(arr.map(async item => (await callback(item)) ? item : fail))).filter(i=>i!==fail)
-}
 function loadPokemon(offset = 0, limit = 25) {
   getAPIData(
     `https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`,
@@ -27,35 +20,6 @@ function loadPokemon(offset = 0, limit = 25) {
     }
   })
 }
-function loadFirePokemon(offset = 0, limit = 25) {
-  getAPIData(
-    `https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`,
-  ).then(async (data) => {
-    for (const pokemon of data.results) {
-      await getAPIData(pokemon.url).then((pokeData) => {
-        console.log(pokeData["types"][0]["type"]["name"])
-        if (data["types"][0]["type"]["name"] === "fire"){
-          //populatePokeCard(data)
-          console.log("true")
-          //console.log(pokeData["types"][0]["type"]["name"])
-        } 
-        //filterSinglePokemonType(pokedata,"fire")
-      //console.log(filter(data.results,test))
-      })
-    }
-  })
-}
-function filterSinglePokemonType(data,filter){
-  //types [{type:{name:"normal"}}]
-
-  if (data["types"][0]["type"]["name"] == filter){
-    //populatePokeCard(data)
-    console.log("true")
-  } 
-}
-//console.log(loadFirePokemon(0,10))
-loadFirePokemon(0,20)
-//loadPokemon(200, 25)
 
 const pokeGrid = document.querySelector('.pokeGrid')
 const loadButton = document.querySelector('.loadPokemon')
@@ -64,9 +28,54 @@ loadButton.addEventListener('click', () => {
   loadPokemon(100, 50)
 })
 
+const allPokemon = await getPokemonType
+
+async function getPokemonType() {
+  const allPokemon = []
+  await getAPIData(
+    `https://pokeapi.co/api/v2/pokemon?limit=1118&offset=0`,
+  ).then(async (data) => {
+    for (const pokemon of data.results) {
+      await getAPIData(pokemon.url).then((pokeData) => {
+        const sortedPokemon = {
+          abilities: pokeData.abilities,
+          height: pokeData.height,
+          id: pokeData.id,
+          name: pokeData.name,
+          types: pokeData.types,
+          weight: pokeData.weight,
+        }
+        allPokemon.push(sortedPokemon)
+        
+        //filterSinglePokemonType(pokedata,"fire")
+      //console.log(filter(data.results,test))
+      })
+    }
+  })
+  return allPokemon
+}
+
+function sortAllPokemonType(type) {
+  return allPokemon.filter((pokemon) => pokemon.types[0].type.name == type)
+}
+// function filterSinglePokemonType(data,filter){
+//   //types [{type:{name:"normal"}}]
+
+//   if (data["types"][0]["type"]["name"] == filter){
+//     //populatePokeCard(data)
+//     console.log("true")
+//   } 
+// }
+// //console.log(loadFirePokemon(0,10))
+// loadFirePokemon(0,20)
+// //loadPokemon(200, 25)
+
+
+
 const fireButton = document.querySelector('.firePokemon')
 fireButton.addEventListener('click', () => {
-
+const pokemonByType = sortAllPokemonType('fire')
+pokemonByType.forEach((item) => populatePokeCard(item))
 })
 
 /* First, get a reference to the pokemon choice button
